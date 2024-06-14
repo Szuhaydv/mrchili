fc156505218cc174eba1f5efeef25b01e3aa0644
szuhaydv <szuhaydv@gmail.com>
2024-06-14 15:30:18 +0200

    main page below fold

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { ProductsComponent } from './products/products.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { MainComponent } from './main/main.component';
 
 export const routes: Routes = [
   { path: 'products', component: ProductsComponent},
   { path: 'about', component: AboutComponent},
   { path: 'contact', component: ContactComponent},
+  { path: '', component: MainComponent},
 ];
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { BelowFoldComponent } from '../below-fold/below-fold.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [BelowFoldComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
